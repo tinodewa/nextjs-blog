19954b176330afd09ddec63cc815612493f4b297
tinodewa <dtino0812@gmail.com>
2024-09-04 09:30:55 +0700

    new brach development

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingmd}>
-        <p>[Your Self Introduction]</p>
+        <p>[Your Self Introduction] this is development only</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
