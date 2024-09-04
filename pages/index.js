f40c263228fbbf72e5c537c148aff4609808b38f
tinodewa <dtino0812@gmail.com>
2024-09-04 09:15:06 +0700

    fix index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import Date from '../components/date';
-import Layout, { siteTitle } from '../components/Layout';
+import Layout, { siteTitle } from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 
 export default function Home({ allPostsData }) {
